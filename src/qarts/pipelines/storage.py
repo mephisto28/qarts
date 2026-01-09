376e92d518366c87ff71e39d0d8a0e151004470f
mephisto28 <462464479@qq.com>
2026-01-09 15:34:07 +0800

    add storage processor for pipeline

---
+++ src/qarts/pipelines/storage.py
@@ -0,0 +1,26 @@
+
+from loguru import logger
+
+from qarts.core import PanelBlockIndexed, PanelBlockDense
+from qarts.loader.dataloader import PanelLoader, VariableLoadSpec
+from .base import Processor, GlobalContext
+
+
+class DataStoreProcessor(Processor):
+
+    def __init__(self, loader: PanelLoader, input_name: str, output_name: str):
+        self.loader = loader
+        self.input_name = input_name
+        self.output_name = output_name
+
+    def process(self, context: GlobalContext):
+        date = context.current_datetime
+        data: PanelBlockDense | PanelBlockIndexed = context.get(self.input_name)
+        if isinstance(data, PanelBlockDense):
+            data = PanelBlockIndexed.from_dense_block(data)
+        
+        save_spec = VariableLoadSpec(var_type='factor', load_kwargs={
+            'factor': self.output_name, 
+            'date': date.date(),
+        })
+        self.loader.save_intraday(data, save_spec)
